e4f2ae99d69effe0aad77e5e470c9556b80d8c97
Pratik Shinde <pratikshinde7262@gmail.com>
2023-01-22 13:49:08 +0530

    Added Unique Email Msg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const connectToMongo=require('./db');
 const express=require('express');
 
-const PORT=3000
+const PORT=5000
 
 connectToMongo();
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,25 +3,38 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
+//Create User Endpont login not required
 router.post(
-  "/",
+  "/createUser",
   [
     body("name", "Enter A Valid Name").isLength({ min: 3 }),
     body("email", "Enter A Valid Email").isEmail(),
-    body("password",'Enter A Password Of Min * Charecters').isLength({ min: 8 }),
+    body("password", "Enter A Password Of Min * Charecters").isLength({
+      min: 8,
+    }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err));
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({
+          err: "Please Enter Unique Email User With This Email Already Exist",
+        });
+      }
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+
+      res.json(user);
+    } catch (error) {
+        res.json(error)
+    }
   }
 );
 
