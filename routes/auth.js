7bc1d59849caa642c5f6bde6ee28de87b433ae6e
Pratik Shinde <pratikshinde7262@gmail.com>
2023-01-31 12:55:23 +0530

    added sucess

--- routes/auth.js
+++ routes/auth.js
@@ -18,14 +18,16 @@ router.post(
     }),
   ],
   async (req, res) => {
+    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success: success, errors: errors.array() });
     }
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res.status(400).json({
+          success: success,
           err: "Please Enter Unique Email User With This Email Already Exist",
         });
       }
@@ -44,12 +46,12 @@ router.post(
           id: user.id,
         },
       };
-
+      success = true;
       const authToken = jwt.sign(data, JWT_SECRET);
 
-      res.json({ authToken });
+      res.json({ success, authToken });
     } catch (error) {
-      res.json(error);
+      res.json({ success, error });
     }
   }
 );
@@ -61,26 +63,27 @@ router.post(
     body("password", "Enter A Password").exists(),
   ],
   async (req, res) => {
+    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success: success, errors: errors.array() });
     }
 
     const { email, password } = req.body;
     try {
       let user = await User.findOne({ email });
       if (!user) {
-        return res.status(400).json({
-          err: "Please Enter Valid Credentials",
-        });
+        return res
+          .status(400)
+          .json({ success: success, err: "Please Enter Valid Credentials" });
       }
 
       const passwordCompare = await bcrypt.compare(password, user.password);
 
       if (!passwordCompare) {
-        return res.status(400).json({
-          err: "Please Enter Valid Credentials",
-        });
+        return res
+          .status(400)
+          .json({ success: success, err: "Please Enter Valid Credentials" });
       }
 
       const data = {
@@ -90,21 +93,23 @@ router.post(
       };
 
       const authToken = jwt.sign(data, JWT_SECRET);
-
-      res.json({ authToken });
+      success = true;
+      res.json({ success: success, authToken });
     } catch (error) {
-      res.json(error);
+      res.json({ success, error });
     }
   }
 );
 
 router.post("/fetchuser", getUser, async (req, res) => {
+  let success=false
   try {
     const userId = req.user.id;
     const user = await User.findById(userId).select("-password");
-    res.status(200).send(user);
+    success=true
+    res.status(200).send({success, user});
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send({success,error});
   }
 });
 
